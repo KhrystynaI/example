f36929fad112e7e7e8a3699bb4b87544decc01da
Dmytro Samodurov <dimasamodurov@gmail.com>
2018-05-03 20:58:08 +0300

    Specify deployment user.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :repository, 'https://github.com/KhrystynaInzhuvatova/example.git'
 set :branch, 'master'
 
 # Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
+set :user, 'deploy'          # Username in the server to SSH to.
 #   set :port, '30000'           # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
