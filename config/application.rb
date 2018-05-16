5d6a84f36b1f635ef1219df40eb5ff1918f81f8f
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-05-17 02:43:54 +0300

    remove key

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,10 @@ module Newspaper
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.enable_dependency_loading = true
-    config.autoload_paths << Rails.root.join('lib')
+
+    #config.enable_dependency_loading = true
+    #config.autoload_paths << Rails.root.join('lib')
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
