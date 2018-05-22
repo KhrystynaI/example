c89120c8a3fa6009be35edd5fed4ff55ac678950
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-05-22 20:37:56 +0300

    change db in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,7 +58,7 @@ task :deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:db_migrate'
+    invoke :'rails:db:drop db:create db:migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
