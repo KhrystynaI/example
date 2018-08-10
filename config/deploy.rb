154a0d25bf74dc89f943546edd7e692cc33436e2
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-08-10 19:22:11 +0300

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :keep_releases, '3'
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 #set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+#set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 set :shared_paths, ['config/database.yml', 'tmp/pids', 'tmp/sockets']
 set :shared_dirs, fetch(:shared_dirs, []).push('log')
 #set :shared_files, fetch(:shared_files, []).push(
