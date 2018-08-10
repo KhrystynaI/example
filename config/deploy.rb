e5a936571862f6ec8148bab6e671b312b4d4d9fd
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-08-10 19:10:55 +0300

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,9 @@ set :branch, 'master'
 set :user, 'ubuntu'           # Username in the server to SSH to.
 #   set :port, '30000'           # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
+set :current_path, 'current' # What file should be the current version. Probably 'www', 'public_html' or perhaps 'current'.
+
+set :keep_releases, '3' 
 
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
@@ -70,11 +73,12 @@ task :deploy do
     invoke :'rails:db_migrate'
     command %{#{fetch(:rails)} db:seed}
     invoke :'rails:assets_precompile'
-    invoke :'deploy:cleanup'
+    #invoke :'deploy:cleanup'
 
     on :launch do
       invoke :'puma:phased_restart'
     end
+    invoke :'deploy:cleanup'
   end
 
   # you can use `run :local` to run tasks on local machine before of after the deploy scripts
