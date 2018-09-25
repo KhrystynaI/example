9760c923903f219b8a54c3bd1d232124e5f17ed2
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-09-25 16:44:55 +0300

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,14 +52,14 @@ end
 task setup: :remote_environment do
   deploy_to   = fetch(:deploy_to)
   shared_path = fetch(:shared_path)
-  command %[touch "#{fetch(:shared_path)}/config/database.yml"]
-  command %[touch "#{fetch(:shared_path)}/config/secrets.yml"]
-  command %[touch "#{fetch(:shared_path)}/config/puma.rb"]
+  command %[ sudo touch "#{fetch(:shared_path)}/config/database.yml"]
+  command %[sudo touch "#{fetch(:shared_path)}/config/secrets.yml"]
+  command %[sudo touch "#{fetch(:shared_path)}/config/puma.rb"]
   comment "Be sure to edit '#{fetch(:shared_path)}/config/database.yml', 'secrets.yml' and puma.rb."
-  command %(mkdir -p "#{fetch(:shared_path)}/tmp/sockets")
-  command %(chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/sockets")
-  command %(mkdir -p "#{fetch(:shared_path)}/tmp/pids")
-  command %(chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/pids")
+  command %( sudo mkdir -p "#{fetch(:shared_path)}/tmp/sockets")
+  command %(sudo chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/sockets")
+  command %(sudo mkdir -p "#{fetch(:shared_path)}/tmp/pids")
+  command %(sudo chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/pids")
   #command %[mkdir -p "#{fetch(:deploy_to)}/current"]
 
   # command %{rbenv install 2.3.0 --skip-existing}
