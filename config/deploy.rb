64b0ded2426da1777511472d4ffadb6bdaf9a966
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-09-25 23:11:29 +0300

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,10 +66,10 @@ task setup: :remote_environment do
   command %[sudo touch "#{fetch(:shared_path)}/config/secrets.yml"]
   command %[sudo touch "#{fetch(:shared_path)}/config/puma.rb"]
   comment "Be sure to edit '#{fetch(:shared_path)}/config/database.yml', 'secrets.yml' and puma.rb."
-  #command %( sudo mkdir -p "#{fetch(:shared_path)}/tmp/sockets")
-  #command %(sudo chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/sockets")
-  #command %(sudo mkdir -p "#{fetch(:shared_path)}/tmp/pids")
-  #command %(sudo chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/pids")
+  command %( sudo mkdir -p "#{fetch(:shared_path)}/tmp/sockets")
+  command %(sudo chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/sockets")
+  command %(sudo mkdir -p "#{fetch(:shared_path)}/tmp/pids")
+  command %(sudo chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/pids")
 
 
   # command %{rbenv install 2.3.0 --skip-existing}
