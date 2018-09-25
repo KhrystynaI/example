b56f69e233674c2fd3b8cdb6b269550718028bf2
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-09-25 23:18:29 +0300

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,10 +31,10 @@ set :user, 'ubuntu'           # Username in the server to SSH to.
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-#set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-#set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 set :shared_paths, ['config/database.yml', 'log']
-set :shared_dirs, fetch(:shared_dirs, []).push('log')
+#set :shared_dirs, fetch(:shared_dirs, []).push('log')
 set :shared_files, fetch(:shared_files, []).push(
   'config/secrets.yml',
   'db/production.sqlite3'
