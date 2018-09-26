69d1285f23c1ac77cd98b0046534c76df94d9d2b
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-09-26 15:30:28 +0300

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -84,6 +84,7 @@ task deploy: :remote_environment do
   deploy do
     comment "Deploying #{fetch(:application_name)} to #{fetch(:domain)}:#{fetch(:deploy_to)}"
     command 'pwd'
+    command "sudo touch deploy.lock"
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     #invoke :'rbenv:load'
