b89ffec205752ce713700c4d76340bf8d2a44d86
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2018-10-02 14:41:58 +0300

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,20 @@ set :user, 'ubuntu'           # Username in the server to SSH to.
 #set :forward_agent, true     # SSH forward_agent.
 #set :term, :system
 #set :execution_mode, :system
-
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock" # accept array for multi-bind
+set :puma_conf, "#{shared_path}/config/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, 1
+set :puma_workers, 1
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, true
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
