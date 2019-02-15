883a1a3ef3e58ad30930bec6f0c122f0ab9c8d8c
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2019-02-15 16:42:50 +0200

    dep

--- config/puma.rb
+++ config/puma.rb
@@ -13,7 +13,7 @@ shared_dir = "#{app_dir}/shared"
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 # Set up socket location
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+bind "unix://#{shared_dir}/sockets/puma.sock"
 # Logging
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 # Set master PID and state locations
